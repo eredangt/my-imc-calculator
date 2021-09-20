ec04bff246f894632747fb9c09a9338676f001ce
eredangt <caio.lopes@estudante.ufla.br>
2021-09-20 14:51:02 -0300

    Feitas modificações solicitadas na tarefa.

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -10,6 +10,7 @@ export class HomePage {
 
   weight: number;
   height: number;
+  situation: string;
 
   constructor(private toastController: ToastController) {}
 
@@ -19,7 +20,22 @@ export class HomePage {
 
   onCalculate() {
     const imc = this.weight / (this.height * this.height);
-    this.showMessage(`IMC = ${imc.toFixed(2)}`);
+    if (imc < 18.5) {
+      this.situation = 'MAGREZA';
+    }
+    else if (imc <= 24.9) {
+      this.situation = 'NORMAL';
+    }
+    else if (imc <= 29.9) {
+      this.situation = 'SOBREPESO';
+    }
+    else if (imc <= 39.9) {
+      this.situation = 'OBESIDADE';
+    }
+    else {
+      this.situation = 'OBESIDADE GRAVE';
+    }
+    this.showMessage(`IMC = ${imc.toFixed(2)} &#8212 ${this.situation}`);
   }
 
   async showMessage(msg: string) {
@@ -31,7 +47,7 @@ export class HomePage {
     const toast = await this.toastController.create(
       {
         message: msg,
-        color: 'light',
+        color: 'tertiary',
         buttons: [
           {
             icon: 'close'
